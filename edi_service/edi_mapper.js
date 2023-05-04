b179156ed018db74ef83f72f4a99b44dba8c8d53
alea91 <73673546+alea91@users.noreply.github.com>
2023-05-04 11:36:01 -0700

    added sc field name in mapping

--- edi_service/edi_mapper.js
+++ edi_service/edi_mapper.js
@@ -6,7 +6,7 @@ const ediMap = (docType) => {
   const fieldMap = mappings[docType];
   fieldMap.fields.forEach((field) => {
     if (field.include) {
-      map[field.label.replace(/\s/g, "")] = field.segment;
+      map[field.oracleField] = field.segment;
     }
   });
 
