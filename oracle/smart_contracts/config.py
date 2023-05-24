5b556231248ec2b312de51466c142e573a060da6
Grant McCaw <grantmccaw@gmail.com>
2023-05-24 10:03:18 -0700

    remove helloworld config code

--- oracle/smart_contracts/config.py
+++ oracle/smart_contracts/config.py
@@ -9,11 +9,9 @@ from algokit_utils import (
     OperationPerformed,
     TransferParameters,
     is_localnet,
-    transfer,
 )
 from algosdk.constants import MIN_TXN_FEE
 from algosdk.transaction import PaymentTxn
-from algosdk.util import algos_to_microalgos
 from algosdk.v2client.algod import AlgodClient
 from algosdk.v2client.indexer import IndexerClient
 
@@ -84,14 +82,14 @@ def deploy(
                 if not suggested_params.flat_fee:
                     if transaction.fee > params.max_fee_micro_algos:
                         raise Exception(
-                            f"Cancelled transaction due to high network congestion fees. "
-                            f"Algorand suggested fees would cause this transaction to cost {transaction.fee} µALGOs. "
-                            f"Cap for this transaction is {params.max_fee_micro_algos} µALGOs."
+                            f"Cancelled transaction due to high network congestion fees. "  # noqa: E501
+                            f"Algorand suggested fees would cause this transaction to cost {transaction.fee} µALGOs. "  # noqa: E501
+                            f"Cap for this transaction is {params.max_fee_micro_algos} µALGOs."  # noqa: E501
                         )
                     if transaction.fee > MIN_TXN_FEE:
                         logger.warning(
                             f"Algorand network congestion fees are in effect. "
-                            f"This transaction will incur a fee of {transaction.fee} µALGOs."
+                            f"This transaction will incur a fee of {transaction.fee} µALGOs."  # noqa: E501
                         )
                 signed_txn = transaction.sign(params.from_account.private_key)
 
@@ -100,43 +98,7 @@ def deploy(
                     f"Called hello on {app_spec.contract.name} ({app_client.app_id}) "
                     f"received: {response.return_value}"
                 )
-        case "HelloWorldApp":
-            app_client = ApplicationClient(
-                algod_client,
-                app_spec,
-                creator=deployer,
-                indexer_client=indexer_client,
-            )
-            deploy_response = app_client.deploy(
-                on_schema_break=(
-                    OnSchemaBreak.ReplaceApp if is_local else OnSchemaBreak.Fail
-                ),
-                on_update=OnUpdate.UpdateApp if is_local else OnUpdate.Fail,
-                allow_delete=is_local,
-                allow_update=is_local,
-            )
 
-            # if only just created, fund smart contract account
-            if deploy_response.action_taken in [
-                OperationPerformed.Create,
-                OperationPerformed.Replace,
-            ]:
-                params = TransferParameters(
-                    from_account=deployer,
-                    to_address=app_client.app_address,
-                    micro_algos=algos_to_microalgos(1),
-                )
-                logger.info(
-                    f"New app created, funding with " f"{params.micro_algos}µ algos"
-                )
-                transfer(algod_client, params)
-
-            name = "world"
-            response = app_client.call("hello", name=name)
-            logger.info(
-                f"Called hello on {app_spec.contract.name} ({app_client.app_id}) "
-                f"with name={name}, received: {response.return_value}"
-            )
         case _:
             raise Exception(
                 f"Attempt to deploy unknown contract {app_spec.contract.name}"
